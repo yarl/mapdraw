c3950ce57e6271da6f80ee643a2b9a54a1337cf5
Paweł Marynowski <yarl@o2.pl>
2013-12-08 20:42:11 +0100

    cleaning in db.js

--- js/db.js
+++ js/db.js
@@ -1,30 +1,50 @@
-var db = {};
-db.info = {};
+var Db = function() {
+    var constructor = function Db() {
+      
+      /**
+       * Map loading
+       * @param String map_id
+       */
+      this.load = function(map_id) {
+        $.ajax({
+          type: "POST",
+          url: "db.php",
+          data: "action=load&id="+map_id,
+          success: function(out) {
+            if(out === "0") {
+              $('#modal .modal-title').html('Brak mapy');
+              $('#modal').modal();
+              return false;
+            }
+            map.loadJSON($.parseJSON(out));
+            return true;
+          }
+        });
+      };
+      
+      /**
+       * Map saving
+       * @param JSON info
+       * @param JSON data
+       */
+      this.save = function(info, data) {
+        if(data === "{}") {
+          $('#modal .modal-title').html('Błąd');
+          $('#modal .modal-body').html('Brak danych do wysłania');
+          $('#modal').modal();
+          return false;
+        }
+        $.ajax({
+          type: "POST",
+          url: "db.php",
+          data: "action=save&author="+auth.user_id+"&info="+encodeURIComponent(info)+"&data="+encodeURIComponent(data),
+          success: function(out) {
+            window.location.pathname = '/yarl/map/'+out;
+          }
+        });
+      };
+    };
 
-db.load = function(map_id, user) {
-  $.ajax({
-    type: "POST",
-    url: _ROOT_+"db.php",
-    data: "action=load&id="+map_id,
-    success: function(out) {
-      if(out === "0") {
-        $('#modal .modal-title').html('Brak mapy');
-        $('#modal').modal();
-        return;
-      }
-      map.loadJSON($.parseJSON(out));
-      return;
-    }
-  });
-};
+    return constructor;
+}();
 
-db.save = function(info, data) {
-  $.ajax({
-    type: "POST",
-    url: _ROOT_+"db.php",
-    data: "action=save&author="+auth.user_id+"&info="+encodeURIComponent(info)+"&data="+encodeURIComponent(data),
-    success: function(out) {
-      alert(out);
-    }
-  });
-};
